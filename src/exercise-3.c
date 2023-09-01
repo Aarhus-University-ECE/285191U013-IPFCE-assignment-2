c424766374e2c108128774ea4508ff37d3c97a54
Pernille Pallesen <60431537+zSORATH@users.noreply.github.com>
2023-09-01 14:06:31 +0200

    Update exercise-3.c, fixed comment

--- src/exercise-3.c
+++ src/exercise-3.c
@@ -2,8 +2,7 @@
 
 // Write a program to compute the Fibonacci numbers. Your program
 // should take an integer n as input from the user, and it then calculates and prints
-// each Fibonacci number from the first Fibonacci number to the nth. For example, a
-// typical execution where the user inputs n = 7 could be:
+// each Fibonacci number from the first Fibonacci number to the nth. 
 
 int main() {
 
